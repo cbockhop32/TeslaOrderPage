b770011ce580d60094e9c445760e20fe5f69746b
Conrad Bockhop <bockhopc@gmail.com>
2021-05-11 14:35:16 -0700

    working version

--- src/Components/ImgSlider/ImgSlider.js
+++ src/Components/ImgSlider/ImgSlider.js
@@ -1,4 +1,6 @@
 import React, {useContext} from 'react';
+import {Carousel} from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import './ImgSlider.scss';
 
 import {carImages } from '../../helper/carimages';
@@ -15,21 +17,37 @@ function ImgSlider() {
 
     let imgList;
 
+    const arrowStyles = {
+        position: 'absolute',
+        top: 'calc(50% - 15px)',
+        zIndex: '2'
+     
+        
+    }
+
 
     // This filters through the carImages  in the helper folder and gets the specific set of images to be rendered in the component
     imgList = carImages.find(car => car.color === currentColor && car.powertrain === currentPowerTrain && currentWheels === car.wheels && car.interior === currentInterior).images
+
+ 
 
 
 
     const handleBack = () => {
         if(currentSlide !== 0) {
             changeSlide(currentSlide - 1)
-        } 
+        } else {
+            changeSlide(imgList.length-1)
+        }
     }
+
+    
 
     const handleForward = () => {
         if(currentSlide !== imgList.length -1 ) {
             changeSlide(currentSlide + 1)
+        } else {
+            changeSlide(0)
         }
     }
 
@@ -38,18 +56,55 @@ function ImgSlider() {
 
     return (
         <div className="ImgSlider">
-                <div className={currentSlide !== 0 && showing === false ? "Slider-Btn": "Slider-Btn Hide"} onClick={() => handleBack()} style={{marginLeft: '1rem'}}>
-                    <i className="fas fa-chevron-left"></i>
-                </div>
-            <div className="ImgSlider-Container">
-              <img src={imgList[currentSlide]} className="Slide" alt="Slide" />
-            </div>
-
-                <div className={currentSlide !== 4 && showing === false ? "Slider-Btn": "Slider-Btn Hide"} onClick={() => handleForward()} style={{marginRight: '1rem'}}>
-                    <i className="fas fa-chevron-right"></i>
-                </div>
+            <Carousel
+          
+                width="100%"
+                showThumbs={false}
+                showStatus={false}
+                showIndicators={false}
+                infiniteLoop={true}
+                selectedItem={currentSlide}
+                swipeable={false}
+ 
+                showArrows={showing ? false : true}
+         
+                renderArrowPrev={(onClickHandler, hasPrev, label) => 
+                    hasPrev && (
+                        <button type="button" className="Slider-Btn" onClick={() => {onClickHandler(); handleBack();}} title={label} style={{...arrowStyles, left: '0', marginLeft: '1rem'}} >
+                            <i className="fas fa-chevron-left"></i>
+                        </button>
+                )}
                 
+
+                renderArrowNext={(onClickHandler = handleForward, hasNext, label) => 
+                    hasNext && (
+                        <button type="button" className="Slider-Btn" onClick={() => {onClickHandler(); handleForward();}} title={label} style={{...arrowStyles,right: '0', marginRight: '1rem'}} >
+                            <i className="fas fa-chevron-right"></i>
+                        </button>
+
+                )}
+            >
+                {imgList.map((image, index) => (<div key={index} className="ImgSlider-Container"><img className="Slide" src={image}/></div>))}
+
+            </Carousel>
         </div>
+
+
+
+
+// {/* 
+//                 <div className={currentSlide !== 0 && showing === false ? "Slider-Btn": "Slider-Btn Hide"} onClick={() => handleBack()} style={{marginLeft: '1rem'}}>
+//                     <i className="fas fa-chevron-left"></i>
+//                 </div>
+//             <div className="ImgSlider-Container">
+//               <img src={imgList[currentSlide]} className="Slide" alt="Slide" />
+//             </div>
+
+//                 <div className={currentSlide !== 4 && showing === false ? "Slider-Btn": "Slider-Btn Hide"} onClick={() => handleForward()} style={{marginRight: '1rem'}}>
+//                     <i className="fas fa-chevron-right"></i>
+//                 </div>
+//                  */}
+       
     )
 }
 
